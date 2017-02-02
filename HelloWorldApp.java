daedd5236c0f542a8f3a9067534e30159607b704
Caleb Paioletti <c.paioletti@gmail.com>
2017-02-02 18:47:12 -0500

    Initial Upload

---
+++ HelloWorldApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ * The HelloWorldApp class implements as application that
+ * simply prints "Hello World!" to standard output.
+ * @author Caleb Paioletti
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!"); //Display the string.
+    }
+    
+}
